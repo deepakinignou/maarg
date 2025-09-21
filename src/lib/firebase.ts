cbfe3f9f9b3875fe7ab68ef571154877e8fdafc8
Deepak <btech41523132@dseu.ac.in>
2025-09-21 07:17:58 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,18 +1,18 @@
 
-import { initializeApp, getApps, getApp } from "firebase/app";
+import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  projectId: "studio-7802347805-97b42",
-  appId: "1:397737367178:web:7f34902d412cc8f2c93f73",
-  storageBucket: "studio-7802347805-97b42.firebasestorage.app",
   apiKey: "AIzaSyBMlW-CvE2jUwZioAFqHno2K9e8sB3GVNI",
   authDomain: "studio-7802347805-97b42.firebaseapp.com",
+  projectId: "studio-7802347805-97b42",
+  storageBucket: "studio-7802347805-97b42.firebasestorage.app",
   messagingSenderId: "397737367178",
+  appId: "1:397737367178:web:7f34902d412cc8f2c93f73"
 };
 
 // Initialize Firebase
-const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
 export { app, auth };
